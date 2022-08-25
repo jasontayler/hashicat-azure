4a2b1addf303a41a7e484f885cd2aaa5eebb27f2
Jason Tayler <jason.tayler@outlook.com>
2022-08-25 03:46:42 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,8 @@
+//--------------------------------------------------------------------
+// Modules
 module "network" {
   source  = "app.terraform.io/jtprod/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "azurerm_resource_group.myresourcegroup.name"
+
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
